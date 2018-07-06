6f761bd36419c31fde4c5571454e75e375a5bdd6
Ian Kigen <ian.gabe.ian@gmail.com>
2018-07-06 15:35:15 +0300

    feat(categories): categories CRUD - add categories views - add categories controller - add categories models

--- app/models/users.rb
+++ app/models/users.rb
@@ -4,6 +4,9 @@ require 'sinatra/activerecord'
 require './config/environments'
 class Users < ActiveRecord::Base
 
+  has_many :categories, dependent: :destroy
+
+
   def set_password=(new_password)
     self.password = BCrypt::Password.create(new_password)
   end
--- config.ru
+++ config.ru
@@ -2,5 +2,7 @@ require 'sinatra'
 
 Dir.glob('./app/{controllers, helpers, models}/*.rb').each { |file| require file }
 
+
 map('/') { run PagesController }
 map('/user') { run UserController }
+map('/learn') { run CategoriesController }
