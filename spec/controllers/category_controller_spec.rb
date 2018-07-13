198a0616cb9b6a4824390332eecd8915a7453c7f
Ian Kigen <ian.gabe.ian@gmail.com>
2018-07-13 12:46:03 +0300

    chore(contoller_spec): add delete category test

--- spec/controllers/category_controller_spec.rb
+++ spec/controllers/category_controller_spec.rb
@@ -47,14 +47,40 @@ describe 'Categories' do
   context 'When user with categories visits the learn page' do
     before do
       post '/login', @valid_login
-      post '/add-category', name: 'Health'
-      get '/learn'
     end
     it 'should fetch articles of that category' do
+      get '/add-category'
+      expect(last_response.body).to include('Select a category')
+      post '/add-category', name: 'Health'
+      get '/learn'
       expect(last_response.body).to include('Health',
                                             'author',
                                             'url',
                                             'description')
+    end
+  end
+
+  context 'When a non logged in user visits delete category page' do
+    before do
+      get '/delete-category/1'
+    end
+    it 'should redirect to login page' do
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_request.path).to eq('/login')
+    end
+  end
+
+  context 'When a logged in user visits delete category page' do
+    before do
+      post '/login', @valid_login
+      post '/add-category', name: 'Health'
+      get "/delete-category/#{Categories.first.id}"
+    end
+    it 'should redirect to login page' do
+      expect(last_response).to be_redirect
+      follow_redirect!
+      expect(last_request.path).to eq('/learn')
     end
   end
 end
